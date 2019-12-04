1a3facf892069f6ad1130c3b606f7635dc0c748d
lottenoorlander <lottenoorlander@gmail.com>
2019-12-04 15:29:33 +0100

    added a new router

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 4000;
-const db = require("./db");
-const Team = require("./team/model");
+// const db = require("./db");
+// const Team = require("./team/model");
+const teamRouter = require("./team/router");
 
+app.use(teamRouter);
 app.listen(port, () => console.log("App is listening to port", port));
---
+++ team/router.js
@@ -0,0 +1,9 @@
+const { Router } = require("express");
+const Team = require("./model");
+const router = new Router();
+
+router.get("/team", (req, res, next) =>
+  Team.findAll().then(teams => res.send(teams).catch(err => next(err)))
+);
+
+module.exports = router;
